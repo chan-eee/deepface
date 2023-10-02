6777fc9e107e19d9d6f8c68f69282b5fb628d26c
chan-eee <117513654+chan-eee@users.noreply.github.com>
2023-10-02 18:48:45 +0900

    Update distance.py

--- deepface/commons/distance.py
+++ deepface/commons/distance.py
@@ -30,7 +30,7 @@ def findThreshold(model_name, distance_metric):
     base_threshold = {"cosine": 0.40, "euclidean": 0.55, "euclidean_l2": 0.75}
 
     thresholds = {
-        "VGG-Face": {"cosine": 0.40, "euclidean": 0.60, "euclidean_l2": 0.86},
+        "VGG-Face": {"cosine": 0.40, "euclidean": 0.60, "euclidean_l2": 0.2},
         "Facenet": {"cosine": 0.40, "euclidean": 10, "euclidean_l2": 0.80},
         "Facenet512": {"cosine": 0.30, "euclidean": 23.56, "euclidean_l2": 1.04},
         "ArcFace": {"cosine": 0.68, "euclidean": 4.15, "euclidean_l2": 1.13},
